913a47cbb54f8943cb449c8e6a75815bbe40b87e
Alejandro <aleperaltabazas@hotmail.com>
2023-08-14 00:15:12 -0300

    added status code setting on response

--- falzar-daemon/src/Falzar/Daemon/API.hs
+++ falzar-daemon/src/Falzar/Daemon/API.hs
@@ -21,10 +21,9 @@ import           Data.String.Interpolate (i)
 import           Data.UUID.V4            (nextRandom)
 import           Falzar.API              (CreateRouteMock (..), DeleteMock (..))
 import           Falzar.Daemon.Context   (Context (dataDirectory, mappedRoutes))
-import           Falzar.Route            (Route (..))
+import           Falzar.Route
 import           GHC.Generics            (Generic)
-import           Network.HTTP.Types      (parseMethod, renderStdMethod,
-                                          status200, status400, status404)
+import           Network.HTTP.Types
 import           Network.Wai             (Request (requestMethod))
 import           System.Directory        (removeFile)
 import qualified Web.Scotty.Reader       as Scotty
@@ -110,6 +109,55 @@ runMocks = do
     Nothing -> do
       json ErrorMessage { message = [i|error: route #{rp} with method #{requestMethod req} is not registered|] }
       Scotty.status status404
-    Just r -> do
-      Scotty.status status200
-      json (fst r).body
+    Just (r, _) -> do
+      Scotty.status (assignStatus r.status)
+      json r.body
+    where
+      -- fuck
+      assignStatus :: Int -> Status
+      assignStatus 200 = status200
+      assignStatus 201 = status201
+      assignStatus 202 = status202
+      assignStatus 203 = status203
+      assignStatus 204 = status204
+      assignStatus 205 = status205
+      assignStatus 206 = status206
+      assignStatus 300 = status300
+      assignStatus 301 = status301
+      assignStatus 302 = status302
+      assignStatus 303 = status303
+      assignStatus 304 = status304
+      assignStatus 305 = status305
+      assignStatus 307 = status307
+      assignStatus 308 = status308
+      assignStatus 400 = status400
+      assignStatus 401 = status401
+      assignStatus 402 = status402
+      assignStatus 403 = status403
+      assignStatus 404 = status404
+      assignStatus 405 = status405
+      assignStatus 406 = status406
+      assignStatus 407 = status407
+      assignStatus 408 = status408
+      assignStatus 409 = status409
+      assignStatus 410 = status410
+      assignStatus 411 = status411
+      assignStatus 412 = status412
+      assignStatus 413 = status413
+      assignStatus 414 = status414
+      assignStatus 415 = status415
+      assignStatus 416 = status416
+      assignStatus 417 = status417
+      assignStatus 418 = status418
+      assignStatus 422 = status422
+      assignStatus 428 = status428
+      assignStatus 429 = status429
+      assignStatus 431 = status431
+      assignStatus 500 = status500
+      assignStatus 501 = status501
+      assignStatus 502 = status502
+      assignStatus 503 = status503
+      assignStatus 504 = status504
+      assignStatus 505 = status505
+      assignStatus 511 = status511
+      assignStatus st  = mkStatus 500 [i|unknown status code #{st}|]
